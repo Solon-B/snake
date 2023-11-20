6a880ff2688853bbd153bd201d56105d9730374c
Solon-B <thorium92@outlook.com>
2023-11-20 14:23:19 -0700

    fixed one of the connecting paths

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
-const config = require('../simon-login/login-simon/dbConfig.json');
+const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
